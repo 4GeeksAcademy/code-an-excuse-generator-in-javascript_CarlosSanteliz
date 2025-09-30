d8129900d0c109db0645777ef756ce33bc1c64c9
Charliekofg <charliekofg@hotmail.com>
2025-09-30 01:14:35 +0000

    Project Done

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ import "./assets/img/4geeks.ico";
 window.onload = function () {
   // //write your code here
   // // console.log("Hello Rigo from the console!");
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
+  let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+  let action = ['ate ', 'peed ', 'crushed ', 'broke '];
+  let what = ['my homework ', 'my phone ', 'the car '];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was busy'];
 
   // for (let i = 0; i < who.length; i++) {
@@ -22,10 +22,17 @@ window.onload = function () {
   //     }
   //   }
   // }
-  let randomWho = Math.floor(Math.random() * who.length);
-  let randomAction = Math.floor(Math.random() * action.length);
-  let randomWhat = Math.floor(Math.random() * what.length);
-  let randomWhen = Math.floor(Math.random() * what.length);
+  function cambiarTexto() {
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
 
-  console.log(who[randomWho] + action[randomAction] + what[randomWhat] + when[randomWhen]);
+    let excuse = who[randomWho] + action[randomAction] + what[randomWhat] + when[randomWhen];
+    console.log(excuse)
+    
+    return excuse;
+
+  }
+  document.getElementById("excuse").textContent = cambiarTexto()
 }
